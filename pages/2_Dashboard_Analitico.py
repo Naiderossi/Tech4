09409fb7af112f94c780f0984a01df8a5f2abd24
Naiderossi <129467530+Naiderossi@users.noreply.github.com>
2025-07-28 16:24:39 -0300

    Update 2_Dashboard_Analitico.py

--- pages/2_Dashboard_Analitico.py
+++ pages/2_Dashboard_Analitico.py
@@ -10,17 +10,18 @@ st.set_page_config(page_title="Painel de Obesidade - Final", layout="wide")
 
 # Carregar dados
 df = pd.read_csv("Obesity_tratado.csv")
+df.columns = df.columns.str.lower()
+df["imc"] = df["weight"] / (df["height"] ** 2)
+df["sedentario"] = df["faf"] == 0
+
+bins = [0, 13, 18, 25, 35, 50, 100]
+labels = ['Crianças', 'Adolescentes', '19-25', '26-35', '36-50', '51+']
+df["faixa_personalizada"] = pd.cut(df["age"], bins=bins, labels=labels, right=False)
+
 from dicionario_variaveis import dicionario_variaveis
 
 # Aplicar renomeações nas colunas para tornar nomes mais intuitivos
 df_renomeado = df.rename(columns=dicionario_variaveis)
-df.columns = df.columns.str.lower()
-df_renomeado["imc"] = df_renomeado["weight"] / (df_renomeado["height"] ** 2)
-df_renomeado["sedentario"] = df_renomeado["faf"] == 0
-
-bins = [0, 13, 18, 25, 35, 50, 100]
-labels = ['Crianças', 'Adolescentes', '19-25', '26-35', '36-50', '51+']
-df_renomeado["faixa_personalizada"] = pd.cut(df_renomeado["age"], bins=bins, labels=labels, right=False)
 
 # Sidebar
 with st.sidebar:
